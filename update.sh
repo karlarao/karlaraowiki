08108770088743d20396d9c0d198990f382f3f54
karlarao <karlarao@gmail.com>
2017-06-21 18:54:50 -0400

    .

---
+++ update.sh
@@ -0,0 +1,13 @@
+cp /Volumes/vm/Dropbox/Documents/KnowledgeFiles/InformationTechnology/TiddlyWiki/karlarao.html index.html
+git config --global user.name "karlarao"
+git config --global user.email "karlarao@gmail.com"
+git add .
+git status # to see what changes are going to be commited
+git commit -m "."
+#git remote add origin git@github.com:karlarao/karlaraowiki.git
+git push origin master
+#git branch gh-pages # this is one time
+git checkout gh-pages # go to the gh-pages branch
+git rebase master # bring gh-pages up to date with master
+git push origin gh-pages # commit the changes
+git checkout master # return to the master branch
